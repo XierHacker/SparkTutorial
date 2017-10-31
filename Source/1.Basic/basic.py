a1da5d6002bd71b9735343c66edb4c5a2dfc535b
xierhacker <xierhacker@163.com>
2017-10-31 10:38:33 +0800

    initial commit

--- Source/1.Basic/basic.py
+++ Source/1.Basic/basic.py
@@ -1,11 +1,11 @@
 from pyspark import SparkConf
 from pyspark import SparkContext
 
-#初始话pyspark
+#
 conf=SparkConf()
 conf.setMaster(value="local").setAppName(value="Leo")
 sc=SparkContext(conf=conf)
 
 
 file=sc.textFile("../../Notes/1.Basic.md")
-print(file.count())
+#print(file.count())
