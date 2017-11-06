bb949c66ea2068d2c5ebfcd7b460f277e514c426
xierhacker <xierhacker@163.com>
2017-11-06 12:09:54 +0800

    initial commit

--- Source/2.RDD/transformation.py
+++ Source/2.RDD/transformation.py
@@ -1,3 +1,12 @@
 from pyspark import SparkConf
 from pyspark import SparkContext
 
+#初始化信息
+conf=SparkConf()
+conf.setMaster(value="local").setAppName(value="transformation_test")
+sc=SparkContext(conf=conf)
+
+
+#基本函数测试
+
+
