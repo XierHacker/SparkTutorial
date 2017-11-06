d0c30b131c9585e3a27ce0ea32ec55e3b078908f
xierhacker <xierhacker@163.com>
2017-11-06 15:48:33 +0800

    initial commit

--- Source/2.RDD/transformation.py
+++ Source/2.RDD/transformation.py
@@ -8,5 +8,26 @@ sc=SparkContext(conf=conf)
 
 
 #基本函数测试
+num1_RDD=sc.parallelize([1,2,3,4,5,6])
+num2_Rdd=sc.parallelize([1,1,2,3,4,5,5,9,11])
+
+#count()
+print("num of num1_RDD :",num1_RDD.count())
+
+#collect
+print("collect of num1_RDD:",num1_RDD.collect())
+
+#countByValue()
+print("countByValue of num2_RDD:",num2_Rdd.countByValue())
+
+
+#take()
+print("take of num2_RDD:",num2_Rdd.take(3))
+
+#takeOerderd()
+print("takeOrderd of num2_RDD:",num2_Rdd.takeOrdered(4))
+
+
+
 
 
